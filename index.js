3fa922bb60f72799803705238da9bf9b82053878
Kayce Heap <kayce.heap@gmail.com>
2022-01-09 19:36:18 -0700

    added the fs to be able to create a html page, added cards from bootstrap

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const inquirer = require("inquirer");
 const Manager = require("./lib/manager.js");
 const Intern = require("./lib/intern.js");
@@ -100,6 +101,64 @@ const addAnother = function () {
         })
 }
 const createhtml = function () {
-    console.log(employees)
+    var html = `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+      <title>Document</title>
+  </head>
+  <body>
+  <h1>The Team</h1>
+  <section class="container">
+      <div class ="row">
+          <div class ="col d-flex flex-row" >`;
+
+    for (var i = 0; i < employees.length; i++) {
+
+       var card = `<div class="card" style="width: 20rem;">
+            <div class="card-body">
+                <h5 class="card-title">${employees[i].getName()}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">${employees[i].getRole()}</h6>
+                <p class="card-text">Id: ${employees[i].getId()}</p>
+                <a href="mailto:${employees[i].getEmail()}" class="card-link">Email: ${employees[i].getEmail()}</a>`;
+                if (employees[i] instanceof Manager) {
+                    const officeNumber = `<p class="card-text">Office Number: ${employees[i].getOfficeNumber()}</p>`;
+                    card += officeNumber;
+                } else if (employees[i] instanceof Intern) {
+                    const school = `<p class="card-text">School Name: ${employees[i].getSchool()}</p>`;
+                    card += school;
+
+                } else if (employees[i] instanceof Engineer) {
+                    const github = `<div><a href="https://github.com/${employees[i].getGithub()}" class="card-link">GitHub: ${employees[i].getGithub()}</a></div>`;
+                    card += github;
+                }
+            
+                
+            const endOfCard =`</div>
+        </div>`
+        card += endOfCard;
+        html += card;
+    }
+    //     <div class="card" style="width: 18rem;">
+    //       <div class="card-body">
+    //         <h5 class="card-title">Card title</h5>
+    //         <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    //         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    //         <a href="#" class="card-link">Card link</a>
+    //         <a href="#" class="card-link">Another link</a>
+    //       </div>
+    //     </div>
+    const end = `</div>
+      </div>
+  </section>
+      
+  </body>
+  </html>`
+
+  html += end;
+  fs.writeFile("./dist/index.html", html, function(err){});
 }
 getEmployee();
